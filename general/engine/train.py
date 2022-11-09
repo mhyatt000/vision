5c85a15497b78bdf7a87bd7e9812e8bab49893a4
Matthew Hyatt <mhyatt000@gmail.com>
2022-11-09 15:49:47 -0600

    deploy

--- general/engine/train.py
+++ general/engine/train.py
@@ -41,7 +41,7 @@ def train_iter(model, loader, trainer):
         trainer.optimizer.step()
         # trainer.scheduler.step()
 
-        acc = sum([y.argmax() == yh.argmax() for y, yh in zip(Y.tolist(), Yh.tolist())]) / len(Y.tolist())
+        acc = sum([y.argmax() == yh.argmax() for y, yh in zip(Y, Yh)]) / len(Y.tolist())
         t.set_description(f'loss: {"%.4f" % loss} | acc: {acc}')
         t.update()
 
