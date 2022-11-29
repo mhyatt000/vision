d9b579e7d39426fff67d5c9478ce9d1e607096a5
matt <mhyatt@luc.edu>
2022-11-29 18:34:58 +0000

    deploy

--- general/losses/__init__.py
+++ general/losses/__init__.py
@@ -6,7 +6,7 @@ from . import arcloss
 from .partial_fc_v2 import PartialFC_V2 as PFC
 
 LOSSES = {
-    "AAM": arcloss.CombinedMarginLoss,
+    "AAM": PFC, # arcloss.CombinedMarginLoss, implements AAM
     "CE": nn.CrossEntropyLoss,
     "PFC": PFC,
 }
