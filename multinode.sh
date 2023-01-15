a16c272941ae0f7172cc1a8c52fd1568df925272
matt <mhyatt@luc.edu>
2023-01-15 01:24:36 +0000

    deploy

--- multinode.sh
+++ multinode.sh
@@ -2,20 +2,9 @@
 
 git add -A ; git commit -m 'deploy' ; git push ;
 
-NODES=("$NODE1" "$NODE2")
-for NODE in "${NODES[@]}"
-do
-    echo $NODE
-    # ssh $NODE eval "cd cs/vision && git pull"
-    ssh $NODE whoami;
-done
-
-HEAD=$(hostname -I);
-echo $HEAD;
-
-# clear;
-# ssh $NODE1 ~/cs/vision/node.sh && \ 
-    # ssh $NODE2 ~/cs/vision/node.sh;
+clear;
+ssh $NODE1 ~/cs/vision/node.sh && \ 
+    ssh $NODE2 ~/cs/vision/node.sh;
 
 # ssh $NODE1 ~/cs/vision/run.sh && ssh $NODE2 ~/cs/vision/run.sh;
 
--- node.sh
+++ node.sh
@@ -4,10 +4,12 @@
 
 whoami && hostname;
 conda activate vision;
+cd cs/vision;
+git pull;
 
-export OMP_NUM_THREADS=10;
-torchrun --nproc_per_node=2 --nnodes=2 --node_rank=0 \
-    --rdzv_id=456 --rdzv_backend=c10d --rdzv_endpoint=$ENVVAR \
-    general/tools/train.py --config-name $1
+# export OMP_NUM_THREADS=10;
+# torchrun --nproc_per_node=2 --nnodes=2 --node_rank=0 \
+    # --rdzv_id=456 --rdzv_backend=c10d --rdzv_endpoint=$ENVVAR \
+    # general/tools/train.py --config-name $1
 
 # torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=12581 general/tools/train.py --config-name $1
