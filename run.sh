889492c8e7b6132eda1effc6d60b7bcc079e2c8f
Matt <mhyatt000@gmail.com>
2023-01-15 13:11:14 -0600

    deploy

--- run.sh
+++ run.sh
@@ -5,8 +5,13 @@
 # git pull:
 # conda activate vision;
 
+if [$NODE1 == hostname]; then
+    GPUS=4
+else
+    GPUS=2
+fi
+
 clear;
-export OMP_NUM_THREADS=10;
-torchrun --standalone --nproc_per_node=$1 general/master.py --config-name $2
+torchrun --standalone --nproc_per_node=$GPUS general/master.py --config-name $1
 
 # torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=12581 general/tools/train.py --config-name $1
