3d053a076ddf3cec44a9f64e1abed82116d748a7
Matt <mhyatt000@gmail.com>
2023-01-15 12:52:08 -0600

    deploy

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 cd ~/cs/vision;
 git pull:
+conda activate vision;
 
 clear;
 export OMP_NUM_THREADS=10;
-torchrun --standalone --nproc_per_node=-1 general/master.py --config-name $1
+torchrun --standalone --nproc_per_node=$1 general/master.py --config-name $2
 
 # torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=12581 general/tools/train.py --config-name $1
