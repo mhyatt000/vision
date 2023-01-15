d5ff10847975eb6bed712d39babee821f5259738
matt <mhyatt@luc.edu>
2023-01-15 04:48:55 +0000

    deploy

--- node.sh
+++ node.sh
@@ -13,7 +13,7 @@ clear;
 
 export OMP_NUM_THREADS=10;
 torchrun --nproc_per_node=$2 --nnodes=2 --node_rank=$1 \
-    --rdzv_id=456 --rdzv_backend=c10d --rdzv_endpoint=$ENVVAR \
-    general/tools/train.py --config-name $3
+    --rdzv_id=456 --rdzv_backend=c10d --rdzv_endpoint=$3 \
+    general/tools/train.py --config-name $4
 
 # torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=12581 general/tools/train.py --config-name $1
