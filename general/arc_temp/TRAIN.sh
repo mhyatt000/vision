06423029b438dd8e11ed9c84bf27db08b912c0af
Matt <mhyatt000@gmail.com>
2023-01-16 13:18:03 -0600

    deploy

--- general/arc_temp/TRAIN.sh
+++
@@ -1,4 +0,0 @@
-clear;
-export OMP_NUM_THREADS=10;
-torchrun --standalone --nproc_per_node=4 ../master.py --config-name symia
-# torchrun --standalone --nproc_per_node=2 train_v2.py configs/wblot_config.py 
