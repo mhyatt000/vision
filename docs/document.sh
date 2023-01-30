14425bb3fdfbe9945af9cc37de303e8bbee8c933
Matthew Hyatt <mhyatt000@gmail.com>
2023-01-29 18:57:22 -0600

    deploy

--- docs/document.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# used to make and update sphinx documentation
-
-sphinx-apidoc -o ./source ../general
-make html
--- docs/source/modules.rst
+++
@@ -1,7 +0,0 @@
-general
-=======
-
-.. toctree::
-   :maxdepth: 4
-
-   general
