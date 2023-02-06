e4dfbe47f2d15d76092d2dae2108b055b82dde91
matt <mhyatt@luc.edu>
2023-02-06 00:34:19 +0000

    deploy

---
+++ general/helpers/tester.py
@@ -0,0 +1,62 @@
+import os
+from general.results import plot
+from general.toolbox.tqdm import prog
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from general.config import cfg
+from general.data import build_loaders, build_loaderx
+from general.helpers import Checkpointer, make_optimizer, make_scheduler
+from general.losses import make_loss
+import torch
+from torch import distributed as dist
+from torch import optim
+from torch.cuda.amp.grad_scaler import GradScaler
+
+
+def gather(x):
+    """simple all gather manuver"""
+
+    if not cfg.distributed:
+        return x
+
+    _gather = [torch.zeros(x.shape, device=cfg.DEVICE) for _ in range(dist.get_world_size())]
+    dist.all_gather(_gather, x)
+    return torch.cat(_gather)
+
+class Tester:
+    """manages and abstracts options from evaluation"""
+
+    def __init__(self, model, loader):
+
+        # essentials
+        self.model = model
+        self.loader = loader
+
+    def embed(self):
+        """docstring"""
+
+        allY, allYh = [],[]
+
+        @prog(len(self.loader), desc="Embed")
+        def _embed(X,Y):
+            X, Y = X.to(cfg.DEVICE), Y.to(cfg.DEVICE)
+            Yh = self.model(X).view((Y.shape[0], -1))
+            Y, Yh = gather(Y), gather(Yh)
+            allY.append(Y)
+            allYh.append(Yh)
+
+        with torch.no_grad():
+            for X, Y in self.loader:
+                _embed(X,Y)
+
+        return torch.cat(allY), torch.cat(allYh)
+
+    def run(self):
+        """docstring"""
+
+        Y,Yh = self.embed()
+        for p in cfg.EXP.PLOTS:
+            plot.PLOTS[p](Y,Yh)
+        
---
+++ general/toolbox/tqdm.py
@@ -0,0 +1,24 @@
+from tqdm import tqdm
+from general.config import cfg
+
+def prog(length, desc=None):
+    """
+    creates a tqdm progress bar for the wrapped function when it is called
+    of length length and desc equal to function output or desc
+    only if there is not one already
+    only if this is a node that can print to stdout
+    iterates the bar on each call ... nice for loops
+    """
+    printnode = not (cfg.rank and cfg.distributed)
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if not hasattr(wrapper, "tqdm"):
+                wrapper.tqdm = tqdm(total=length, desc=desc) #, leave=False)
+            result = func(*args, **kwargs)
+            if result:
+                wrapper.tqdm.set_description(result)
+            wrapper.tqdm.update()
+
+            return result
+        return wrapper if printnode else func  # no tqdm if not printnode
+    return decorator
