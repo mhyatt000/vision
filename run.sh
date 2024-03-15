236dd5229c4a2eae2304a2caa12afc55f71db642
Matthew Hyatt <mhyatt000@gmail.com>
2024-03-15 11:38:36 -0500

    deploy

--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-if [ $# -gt 0 ]; then
-  arg1="$1"
-else
-  arg1="--config-file"
-fi
-
-
 export OMP_NUM_THREADS=62;
-torchrun --standalone --nproc_per_node gpu ~/cs/vision/general/master.py --config-file $1
+torchrun --standalone --nproc_per_node gpu ~/cs/vision/general/master.py $@
 
 # torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=12581 general/tools/train.py --config-file $1
