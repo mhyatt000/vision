b0593e53ea74766bdc88abc41798246ea84305ec
Matthew Hyatt <mhyatt000@gmail.com>
2024-03-21 01:26:21 -0500

    nvim deploy

--- general/results/dprime.py
+++ general/results/dprime.py
@@ -14,9 +14,9 @@ class DPrimePlotter(Plotter):
     def calc(self, Y, Yh):
         """measures if positive pairs are different from negative pairs"""
 
-        phist = {i: [] for i in range(cfg.loader.data.n_classes)}
-        nhist = {i: [] for i in range(cfg.loader.data.n_classes)}
-        C = set(range(cfg.loader.data.n_classes))
+        phist = {i: [] for i in range(self.cfg.loader.data.n_classes)}
+        nhist = {i: [] for i in range(self.cfg.loader.data.n_classes)}
+        C = set(range(self.cfg.loader.data.n_classes))
 
         Y = Y.view(-1)
 
