4229f547f8c3e6dd8ebcd7b5fc436695760cf7a6
Matthew Hyatt <mhyatt000@gmail.com>
2024-03-21 05:01:34 -0500

    nvim deploy

--- general/results/cam.py
+++ general/results/cam.py
@@ -119,8 +119,9 @@ class CAMPlotter(Plotter):
                     fname = osp.join("cam", self.classes[label], PATH[i])
                     self.mkfig(fname)
 
-            for X, Y, PATH in testloader.items():
-                    _cam(X, Y)
+            for i in iter(testloader):
+                X, Y, PATH = i.items()
+                _cam(X, Y)
 
     def show(self, *args, **kwargs):
         """images are already plotted in calc because it is memory intensive"""
