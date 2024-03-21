2e9508d9bb4ceb2fc18b2d453559faf11b0fc8e3
Matthew Hyatt <mhyatt000@gmail.com>
2024-03-21 03:27:04 -0500

    nvim deploy

--- general/results/cam.py
+++ general/results/cam.py
@@ -1,5 +1,4 @@
 """pip install grad-cam"""
-
 import os
 import os.path as osp
 
@@ -16,6 +15,7 @@ from torch import nn
 from torchvision import transforms
 from torchvision.io import read_image
 from torchvision.models import resnet50
+from tqdm import tqdm
 
 from general import config
 from general.models import build_model
@@ -29,8 +29,8 @@ class CAMPlotter(Plotter):
 
     def mk_layers(self, model):
         # hard coded for now
-        
-        if self.cfg.model.body == 'ffc':
+
+        if self.cfg.model.body == "ffc":
             main_layers = [model.layer1, model.layer2, model.layer3, model.layer4]
 
             real = lambda x: not isinstance(x, nn.Identity)
@@ -43,7 +43,7 @@ class CAMPlotter(Plotter):
 
             self.layer_groups = [global_layers, local_layers, all_layers]
 
-        elif self.cfg.model.body == 'srnet':
+        elif self.cfg.model.body == "srnet":
             self.layer_groups = [model.block1, model.block2, model.block3, model.block4]
             self.layer_groups += [self.layer_groups]
 
@@ -60,12 +60,11 @@ class CAMPlotter(Plotter):
         dist.all_gather(_gather, x)
         return torch.cat(_gather)
 
-    def calc(self, *args, model=None, trainloader=None , **kwargs):
-
+    def calc(self, *args, model=None, trainloader=None, **kwargs):
         self.mk_layers(model)
 
         for layer in self.layer_groups:
-            allY, allgcam, allX = [], [], []  
+            allY, allgcam, allX = [], [], []
 
             # Construct the CAM object once, and then re-use it on many images
             cam = GradCAM(model=model, target_layers=layer)
@@ -76,8 +75,8 @@ class CAMPlotter(Plotter):
                 Y = Y.to(self.cfg.rank, non_blocking=True)
 
                 gcam = cam(input_tensor=X, eigen_smooth=False)
-                allgcam.append(gcam.cpu())  
-                allX.append(X.cpu())  
+                allgcam.append(gcam.cpu())
+                allX.append(X.cpu())
                 allY.append(Y.cpu())
 
             for X, Y in testloader:
@@ -97,23 +96,25 @@ class CAMPlotter(Plotter):
                 # Plotting
                 fig, axs = plt.subplots(1, 3, figsize=(15, 5))
                 axs[0].imshow(image_np)
-                axs[0].set_title('Original Image')
+                axs[0].set_title("Original Image")
                 axs[1].imshow(visualization)
-                axs[1].set_title('CAM Visualization')
+                axs[1].set_title("CAM Visualization")
                 axs[2].imshow(gcam.numpy(), cmap="jet", alpha=0.5)
-                axs[2].set_title('CAM Heatmap')
+                axs[2].set_title("CAM Heatmap")
 
                 for ax in axs:
-                    ax.axis('off')  # Turn off axis
+                    ax.axis("off")  # Turn off axis
 
                 # Save figure with a temporary filename
-                fname = ''
-                with tempfile.NamedTemporaryFile(delete=False, suffix='.png', prefix='plot_', dir='./') as tmpfile:
+                fname = ""
+                with tempfile.NamedTemporaryFile(
+                    delete=False, suffix=".png", prefix="plot_", dir="./"
+                ) as tmpfile:
                     fname = tmpfile.name
 
                 out.get_path(self.cfg)
-                os.mkdir(osp.join(self.cfg.exp.out, 'cam'))
-                fname = osp.join('cam',fname)
+                os.mkdir(osp.join(self.cfg.exp.out, "cam"))
+                fname = osp.join("cam", fname)
                 self.mkdir(fname)
 
     def show(self, *args, **kwargs):
